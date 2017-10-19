eebbbeccfedb39f1a86d71fefde8d4a50a0a2ffb
wangbiaozy <56565283@qq.com>
2017-10-19 16:55:26 +0800

    1

--- js/index.js
+++ js/index.js
@@ -76,22 +76,80 @@ $(function () {
             }
         });
     }
-
     //首页抽奖按钮
+    var f_;
     $('.cjBtn').on('click',function () {
         if($(this).hasClass('active')){
             return false;
         }else{
             $(this).addClass('active');
+            $('.n1').addClass('act');
+            $('.n2').addClass('act');
+            $('.n3').addClass('act');
+            flag = true;
+            showTc_1();
+            if($(this).hasClass('_1')){
+                f_ = 1;
+            }else if($(this).hasClass('_2')){
+                f_ = 2;
+            }else{
+                f_ = 3;
+            }
         }
+    });
+    //弹出老虎机弹窗
+    function showTc_1() {
+        $('.tc_1').fadeIn();
+        showMask();
+    }
+    //关闭老虎机弹窗
+    function hideTc_1() {
+        $('.tc_1').fadeOut();
+        hideMask();
+    }
+    //老虎机抽奖按钮
+    var flag = true;
+    $('.tcCjBtn').on('click',function () {
+        var $n1 = $('.n1'),
+            $n2 = $('.n2'),
+            $n3 = $('.n3'),
+            nums_a = Math.floor(Math.random()*10),
+            nums_b = Math.floor(Math.random()*10),
+            nums_c = Math.floor(Math.random()*10);
+        if(flag){
+            $n3.removeClass('act');
+            $n3.children().text(nums_a);
+            var timer1 = window.setTimeout(function () {
+                $n2.removeClass('act');
+                $n2.children().text(nums_b);
+            },1000);
+            var timer2 = window.setTimeout(function () {
+                $n1.removeClass('act');
+                $n1.children().text(nums_c);
+            },2000);
+            var timer3 = window.setTimeout(function () {
+                hideTc_1();
+                if(f_ == 1){
+                    $('.mbBox div:nth-child(1) span').text(nums_c.toString()+nums_b.toString()+nums_a.toString());
+                }else if(f_ == 2){
+                    $('.mbBox div:nth-child(2) span').text(nums_c.toString()+nums_b.toString()+nums_a.toString());
+                }else if(f_ == 3){
+                    $('.mbBox div:nth-child(3) span').text(nums_c.toString()+nums_b.toString()+nums_a.toString());
+                }
+            },2500);
+            flag = false;
+        }
+
     });
     //首页再玩一次按钮动画
     window.setInterval(function () {
         $('footer div').animate({
-            opacity: '0'
-        },250,function () {
+            width: '50%',
+            height: '80%'
+        },500,function () {
             $('footer div').animate({
-                opacity: '1'
+                width: '100%',
+                height: '100%'
             },500);
         });
     },1000);
